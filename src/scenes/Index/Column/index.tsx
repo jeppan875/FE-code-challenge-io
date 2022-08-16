3d4952636741afc8f2da8cbcd05f2083aac88897
Jesper Hägglund <jeppan875@gmail.com>
2022-08-16 07:19:39 +0200

    column title

--- src/scenes/Index/Column/index.tsx
+++ src/scenes/Index/Column/index.tsx
@@ -2,8 +2,13 @@ import type { ReactNode } from "react";
 
 import styles from "./styles.module.css";
 
-type Props = { children?: ReactNode };
+type Props = { children?: ReactNode; title: string };
 
-export function Column({ children }: Props) {
-  return <div className={styles.container}>{children}</div>;
+export function Column({ children, title }: Props) {
+  return (
+    <div>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.container}>{children}</div>
+    </div>
+  );
 }
--- src/scenes/Index/index.tsx
+++ src/scenes/Index/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Status, useJobsQuery } from "../../generated/graphql";
 import { Card } from "./Card";
 import { Column } from "./Column";
@@ -18,21 +17,21 @@ export function Index() {
 
   return (
     <div className={styles.container}>
-      <Column>
+      <Column title="To do">
         {data.jobs
           .filter((it) => it.status === Status.ToDo)
           .map((it) => (
             <Card {...it} key={it.id} />
           ))}
       </Column>
-      <Column>
+      <Column title="In progress">
         {data.jobs
           .filter((it) => it.status === Status.InProgress)
           .map((it) => (
             <Card {...it} key={it.id} />
           ))}
       </Column>
-      <Column>
+      <Column title="Done">
         {data.jobs
           .filter((it) => it.status === Status.Done)
           .map((it) => (
